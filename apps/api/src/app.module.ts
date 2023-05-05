6d407cb2d2a14fa60e06871a72f2a57a99dbfee4
Kjartan Dige <emildige@gmail.com>
2023-05-05 18:33:43 +0200

    added teams module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { dbConfig } from '@qvik/db';
 import { UsersModule } from './users/users.module';
+import { TeamsModule } from './teams/teams.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { UsersModule } from './users/users.module';
       ...dbConfig,
     }),
     UsersModule,
+    TeamsModule,
   ],
   controllers: [],
   providers: [],
