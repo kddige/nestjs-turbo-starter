7f72bdf2a5a084638da53a09d43703fdf7293109
Kjartan Dige <emildige@gmail.com>
2023-05-05 18:36:23 +0200

    added @nestjs/swagger

--- apps/api/src/teams/teams.module.ts
+++ apps/api/src/teams/teams.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TeamsService } from './teams.service';
 import { TeamsController } from './teams.controller';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { TeamEntity } from '@qvik/db';
 
 @Module({
+  imports: [MikroOrmModule.forFeature([TeamEntity])],
   providers: [TeamsService],
-  controllers: [TeamsController]
+  controllers: [TeamsController],
 })
 export class TeamsModule {}
